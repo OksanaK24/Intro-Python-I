078af50a19b62814e258f67e7d6c03bedc9beb80
Oksana Kril <oksanakril@Oksanas-iMac.local>
2020-06-07 13:06:20 -0500

    funcrions and args are done

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,6 +1,10 @@
 # Write a function is_even that will return true if the passed-in number is even.
 # YOUR CODE HERE
 
+def is_even(x):
+    if(x%2 == 0):
+        return "true"
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
@@ -8,8 +12,13 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 # YOUR CODE HERE
 
-# Read a string from the keyboard
-s = input("Enter some characters: ")
+if(is_even(num)):
+    print("Even!")
+else:
+    print("Odd")
 
-# Write a function that reverses the input string s
-# YOUR CODE HERE
+# # Read a string from the keyboard
+# s = input("Enter some characters: ")
+
+# # Write a function that reverses the input string s
+# # YOUR CODE HERE
